06307f28d95ef13d475fe88b3cc349b16d078a27
Sergii Iukhymchuk <Sergii.Iukhymchuk83@gmail.com>
2018-01-30 14:45:52 +0200

    Update jenkins

--- contact/settings.py
+++ contact/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                  'django_jenkins.tasks.run_pep8',
                  'django_jenkins.tasks.run_pyflakes',
-                 'django_jenkins.tasks.with_coverage',
                  'django_jenkins.tasks.django_tests',)
 
 # Use nose to run all tests
