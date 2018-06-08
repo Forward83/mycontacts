69f91ca8be24d78920c8d41207ecf77ef3bf667b
Sergii Iukhymchuk <Sergii.Iukhymchuk83@gmail.com>
2018-06-08 11:42:22 +0300

    Correct serializer from dev

--- api/serializers.py
+++ api/serializers.py
@@ -7,5 +7,5 @@ class ContactSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Contact
-        fields = ('url', 'id', 'owner', 'firstname', 'secondname', 'lastname', 'mobile', 'home_phone', 'address',
-                  'email', 'star')
+        fields = ('url', 'id', 'owner', 'firstname', 'secondname', 'lastname', 'mobile', 'personal_phone',
+                  'business_phone', 'company', 'position', 'address', 'email', 'star')
